56bcab3001e0d67faa03b13148055d7822d7525a
Aaron Wang <FallenPuppet@yeah.net>
2019-08-14 17:15:47 +0800

    Spring Cloud Demo

--- common-provider-8001/src/main/java/com/anlsj/springcloud/Provider8001Application.java
+++ common-provider-8001/src/main/java/com/anlsj/springcloud/Provider8001Application.java
@@ -3,9 +3,11 @@ package com.anlsj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Provider8001Application {
 
     public static void main(String[] args) {
