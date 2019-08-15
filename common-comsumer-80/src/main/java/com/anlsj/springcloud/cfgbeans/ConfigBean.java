ef13ea919f1a176af676b23f1aa7fb68f9f0ef72
Aaron Wang <FallenPuppet@yeah.net>
2019-08-16 07:39:00 +0800

    Spring Cloud Demo

--- common-comsumer-80/src/main/java/com/anlsj/springcloud/cfgbeans/ConfigBean.java
+++ common-comsumer-80/src/main/java/com/anlsj/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.anlsj.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,11 @@ public class ConfigBean {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule getRule(){
+        return new RandomRule();
     }
 
 }
