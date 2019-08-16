8347bc5abdb03959b74454a0e92df4c385a3b3f0
Aaron Wang <FallenPuppet@yeah.net>
2019-08-16 13:47:27 +0800

    Spring Cloud Demo

--- common-comsumer-80/src/main/java/com/anlsj/springcloud/Consumer80Application.java
+++ common-comsumer-80/src/main/java/com/anlsj/springcloud/Consumer80Application.java
@@ -1,11 +1,14 @@
 package com.anlsj.springcloud;
 
+import com.anlsj.ribbon.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="MICROSERVICE-DEPT",configuration = MySelfRule.class)
 public class Consumer80Application {
     public static void main(String[] args) {
         SpringApplication.run(Consumer80Application.class, args);
