9caf1aef2edeea0cd94368a580815297518c3980
FallenPuppet <FallenPuppet@yeah.net>
2019-08-20 20:15:49 +0800

    Spring Cloud Demo

--- common-consumer-feign/src/main/java/com.anlsj/springcloud/ConsumerFeignApplication.java
+++ common-consumer-feign/src/main/java/com.anlsj/springcloud/ConsumerFeignApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication()
+@SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class ConsumerFeignApplication {
